de5c898b93bfe38bc3e34c7107441fa5864a8f9b
임정훈 <ssep4u@gmail.com>
2024-05-27 15:03:58 +0900

    급식 API 비동기 호출해서 급식 정보 가져오자

--- 240513_nav/js/school-food.js
+++ 240513_nav/js/school-food.js
@@ -1,5 +1,11 @@
+const API_KEY = "e323d599705a47c2ad3a8a579bc7b2ac"; //학교 급식 API 키
+const URL = "https://open.neis.go.kr/hub/mealServiceDietInfo";  //학교 급식 API URL
+const ATPT_OFCDC_SC_CODE = "B10";   //서울특별시교육청
+const SD_SCHUL_CODE = "7011569";    //미림마이스터고등학교
+
 let currentDate = new Date();   // 현재 날짜를 저장한다.
 
+//급식 정보 제목 표시하자
 const displayDate = () => {
     let days = "일월화수목금토";    // 요일
     let month = currentDate.getMonth() + 1; // 현재 달 불러오기
@@ -12,11 +18,32 @@ const displayDate = () => {
     schoolFoodTitleHeader.innerText = titleText;
 }
 
+//급식 정보 날짜 바꾸자
 const changeDate = (diff) => {
     currentDate.setDate(currentDate.getDate() + diff);
+    displayDate();  // 화면에 변경된 날짜를 표시
 
     const dateData = currentDate.toISOString().slice(0, 10).replace(/-/g, ""); 
     // 2024-05-23 -> 20240523 'YYYYMMDD'
-
-    displayDate();  // 화면에 변경된 날짜를 표시
+    getSchoolFoodMenu(dateData);
 }
+
+//급식 API 이용해서 급식 정보 받아오자
+const getSchoolFoodMenu = (dateData) => {
+    let url = `${URL}?Type=json&KEY=${API_KEY}\
+&pIndex=1\
+&pSize=100\
+&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}\
+&SD_SCHUL_CODE=${SD_SCHUL_CODE}\
+&MLSV_YMD=${dateData}`;
+
+    //비동기로 url 호출
+    //error 없다면 then 함수 호출되고, response.json()으로 실제 데이터만 가져오자
+    //error 있다면 catch 함수 호출되고, 에러 출력하자
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+}
+
+//받아온 급식 정보 웹사이트에 표시하자
