6542a0d24b308e58247b586b0a811528794d0698
임정훈 <ssep4u@gmail.com>
2024-11-04 15:08:40 +0900

    기숙사 필요한 초기 데이터 요청해서 응답 => js 변수

--- 240513_SchoolMarketDormitory/js/dormitory.js
+++ 240513_SchoolMarketDormitory/js/dormitory.js
@@ -13,6 +13,42 @@ const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomName
 // selectionRoomNameDiv.style.display = "block";
 // boardDiv.style.display = "block";
 
+let allData;    //모든 초기화 정보: 세탁기, 시간, 호실 정보
+let weeklyReservation;  //미리 요일별로 지정된 예약 정보
+let newReservation;     //사용자가 입력하고 있는 예약 정보
+let reservations;       //사용자가 예약 완료한 정보들
+
+const initData = async () => {
+    //allData 가져오자
+    const getAllData = async (url) => {
+        return fetch(url)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error(error.message));
+    }
+
+    //weeklyReservation 가져오자
+    const getWeeklyReservation = async (url) => {
+        try{
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    
+    allData = await getAllData("js/allData.json");
+    console.log(allData);
+    
+    weeklyReservation = await getWeeklyReservation("js/weekly-reservation.json");
+    console.log(weeklyReservation);
+    
+}
+
+
+
+
 const setPage = (page) => {
     //clear selection
     for (const selectionItemDiv of selectionItemDivs) {
@@ -32,4 +68,5 @@ const setPage = (page) => {
     //show page
     pageDivs[page-1].style.display = "block";       //1페이지: calendar, 2페이지: swt, 3페이지: srn, 4페이지: board
 }
+initData();
 setPage(1);
